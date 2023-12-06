f2a12e2ec5d2355c6c467702988715eac2edadc6
paul <leebosuk89@gmail.com>
2023-12-06 20:49:20 +0900

    23-12-05 login, cookie, session

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -43,7 +45,7 @@ public class HomeController {
 
     }
 
-    @GetMapping("/")
+    /*@GetMapping("/")*/
     public String homeLoginV2(HttpServletRequest request, Model model){
 
         //세션 관리자에 저장된 회원 정보 조회
@@ -58,5 +60,37 @@ public class HomeController {
         return "loginHome";
 
     }
+    /*@GetMapping("/")*/
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+        //세션이 없으면 home
+        HttpSession session = request.getSession(false); //세션을 받을 의도가없기때문에 false
+        if (session == null) {
+            return "home";
+        }
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        //세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false)
+            Member loginMember,
+            Model model) {
+        //세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember); //여기서는 member객체를 다넘기지만 실무에서는 로그인전용 member객체를 따로 생성하여서 최소한의 필드만 넘기는것이좋다.
+        return "loginHome";
+    }
+
 }
 
